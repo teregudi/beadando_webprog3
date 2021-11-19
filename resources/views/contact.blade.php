221857994c8d522ef09b8963a35b3abf563192a9
teregudi <teregudi.tamashi@gmail.com>
2021-11-19 13:35:38 +0100

    form csrf added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
     <h1>Contact page</h1>
+
+    <form method="POST" action="{{ route('contact-form-submit') }}">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email address">
+        </div>
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" class="form-control" id="subject" name="subject">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/contact', function() {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/about', function() {
     return view('about');
-});
+})->name('about');
+
+Route::post('/contact/submit', function() {
+    return Request::all();
+})->name('contact-form-submit');
